5507beda4923e2fa6ecc194b8d81dd6be75743c9
piotrulanowski <piotr.ulanowski@gmail.com>
2021-07-01 00:06:52 +0200

    20210630 - TODO : An error occurred (InvalidBucketName) when calling the CreateBucket operation: The specified bucket is not valid.

--- src/create_s3.py
+++ src/create_s3.py
@@ -4,8 +4,56 @@ import configparser
 import logging
 import os.path
 from botocore.exceptions import ClientError
-logging = logging.getLogger().setLevel(logging.INFO)
+log = logging.getLogger()
+
 
 def config_exists():
-    if os.path.isfile('main.cfg') == True:
+    if os.path.isfile('C:\\Users\\pit\\Google Drive\\Data Engineering\\Projects\\amazon_reviews\\main.cfg') == True:
         return True
+    else:
+        log.warning('config file missing')
+
+
+def config_read():
+    config = configparser.ConfigParser()
+    config.read('C:\\Users\\pit\\Google Drive\\Data Engineering\\Projects\\amazon_reviews\\main.cfg')
+    return config
+
+def s3_client_start():
+    config = config_read()
+
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id = config['AWS']['ID'],
+        aws_secret_access_key = config['AWS']['SECRET'],
+        aws_session_token = config['AWS']['TOKEN'],
+        region_name = config['AWS']['REGION']
+    )
+    return s3_client
+
+def create_buckets():
+    config = config_read()
+    s3_client = s3_client_start()
+    bucket_name = [config['S3BUCKET']['NAME_RAW'],config['S3BUCKET']['NAME_FINAL']]
+
+    try:
+        bucket_check = s3_client.list_buckets()
+        buckets = [bucket['Name'] for bucket in bucket_check['Buckets']]
+
+        for name in bucket_name:
+            if (bucket_name in buckets):
+                log.warning('Bucket {} already exists'.format(name))
+            else:
+                s3_client.create_bucket(Bucket = name)
+                log.info('Bucket {} created'.format(name))
+
+    except ClientError as e:
+        log.error(e)
+        return False
+    return True
+
+if __name__=="__main__":
+    config_exists()
+    config_read()
+    s3_client_start()
+    create_buckets()
--- tests/test_s3.py
+++ tests/test_s3.py
@@ -1,5 +1,31 @@
 from src.create_s3 import config_exists
+from src.create_s3 import config_read
+from src.create_s3 import s3_client_start
+
 
 #check if config exists
 def test_config_exists():
-    assert config_exists() == True
+    """
+    GIVEN creating S3 bucket process starts
+    WHEN try to fetch credentials
+    THEN file with credentials exists
+    """
+    assert (config_exists() == True), 'config file not present in project'
+
+def test_config_read():
+    """
+    GIVEN configuration file exists
+    WHEN config is read
+    THEN all values are available for further usage
+    """
+    config = config_read()
+    assert (config['AWS']['REGION'] == 'us-east-1'), "config value for AWS REGION is incorrect"
+
+def test_s3_client_start():
+    """
+    GIVEN configuration file is available
+    WHEN creating boto3 session for S3
+    THEN function is returning s3 client cursor
+    """
+    s3_client = s3_client_start()
+    assert s3_client is not None
